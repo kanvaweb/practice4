2a315b8a9ec544a181f08c4509f30bb5195665b1
Andrey Kislitsyn <kanvaweb@gmail.com>
2022-10-13 17:56:50 +0700

    Task29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+// ввод элеменетов через консоль (через запятую. использовать метод string.Split())
+
+Console.Write("Задача 29. Введите числа через запятую: ");
+string numbers = Console.ReadLine();
+string[] array = numbers.Split(',');
+int result = 0;
+
+for (int i = 0; i < array.Length; i++) 
+    result = result + int.Parse(array[i].ToString());
+
+Console.WriteLine($"{numbers} -> {result}");
