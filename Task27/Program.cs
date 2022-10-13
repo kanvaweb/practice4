11247db87d629033d57068fecb9166fd3bbbdad8
Andrey Kislitsyn <kanvaweb@gmail.com>
2022-10-13 17:41:55 +0700

    Task27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Задача 27. Введите число, чтобы получить сумму цифр в числе");
+
+Console.Write("Число: ");
+int number = int.Parse(Console.ReadLine());
+int result = 0;
+
+for (int i = 0; i < number.ToString().Length; i++) 
+{
+    result = result + int.Parse(number.ToString()[i].ToString());
+}
+
+Console.WriteLine($"{number} -> {result}");
